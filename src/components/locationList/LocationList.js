c7a3da0a78394eefe14181c44d2defc027a3782d
Nick Hansen <nick.hansen13@gmail.com>
2019-01-17 11:28:36 -0600

    exercise code

---
+++ src/components/locationList/LocationList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class LocationList extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Location List</h1>
+                <section>North Nashville</section>
+                <p>123 North Street</p>
+                <section>South Nashville</section>
+                <p>456 outh Street</p>
+            </article>
+        );
+    }
+}
