08feb1edeede7ea11490caec55a372e1818f85c0
Nick Hansen <nick.hansen13@gmail.com>
2019-01-17 15:26:14 -0600

    code for chapter 2 of react

--- src/components/locationList/LocationList.js
+++ src/components/locationList/LocationList.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react'
 export default class LocationList extends Component {
     render() {
         return (
-            <article>
-                <h1>Location List</h1>
-                <section>North Nashville</section>
-                <p>123 North Street</p>
-                <section>South Nashville</section>
-                <p>456 outh Street</p>
-            </article>
+            <section className = "locations">
+            {
+                this.props.locations.map(location =>
+                    <div key={location.id}>
+                        {location.name}
+                        {location.address}
+                    </div>
+                )
+            }
+            </section>
         );
     }
 }
