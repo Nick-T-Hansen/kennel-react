0bfc93eac4b5cb526ece8fecfd843aaadb1b66e2
Nick Hansen <nick.hansen13@gmail.com>
2019-01-18 15:51:49 -0600

    added delete functionality using react

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import "./Employee.css"
+import dog from "./DogIcon.png"
 
 
-class EmployeeList extends Component {
-    render() {
-        console.log(this.props.employees)
+export default class EmployeeList extends Component {
+    render () {
         return (
             <section className="employees">
             {
                 this.props.employees.map(employee =>
-                    <div key={employee.id}>
-                        {employee.name}
+                    <div key={employee.id} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                <img src={dog} className="icon--dog" />
+                                {employee.name}
+                                <a href="#"
+                                    onClick={() => this.props.deleteEmployee(employee.id)}
+                                    className="card-link">Delete</a>
+                            </h5>
+                        </div>
                     </div>
                 )
             }
@@ -17,5 +26,3 @@ class EmployeeList extends Component {
         )
     }
 }
-
-export default EmployeeList
